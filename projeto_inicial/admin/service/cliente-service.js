eecc7ebd58fe26e76ab7e61b561570b7bec54a4a
Ially Fabiane <iallyfabiane@gmail.com>
2022-04-13 10:40:09 -0300

    adicionando código-fonte

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -18,13 +18,29 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela]') //requisitando o elemento HTML com o data-attribute data-tabela
 
-const http = new XMLHttpRequest() //Inicializando o objeto XMLHttpRequest
-http.open('GET', 'http://localhost:3000/profile') //abrir a comunicação entre a aplicação e a API
-http.send() //enviando a requisição
-//o que vou fazer com a resposta que o servidor (requisição AJAX) me enviar de volta. No caso do onload, ao carregar a página
-http.onload = () => {
-    const data = JSON.parse(http.response) //criando um objeto com o método JSON.parse() para transformar a resposta http que é uma string em objeto JavaScript
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
- })} 
-// percorrendo o objeto com o método forEach() e adicionando a função criaNovaLinha(que cria <tr>) ao elemento pai <tbody> com o método appendChild()
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => { // criando uma promessa
+
+        const http = new XMLHttpRequest() //Inicializando o objeto XMLHttpRequest
+        http.open('GET', 'http://localhost:3000/profile') //abrir a comunicação entre a aplicação e a API
+        http.onload = () => {  //o que vou fazer com a resposta que o servidor (requisição AJAX) me enviar de volta. No caso do onload, ao carregar a página
+            // realização de validação da resposta http prometida utilizando if...else
+            if (http.status >= 400) {
+                reject(JSON.parse(http.response)) //criando um objeto com o método JSON.parse() para transformar a resposta http que é uma string em objeto JavaScript
+            } else {
+                resolve(JSON.parse(http.response)) //criando um objeto com o método JSON.parse() para transformar a resposta http que é uma string em objeto JavaScript
+            }
+        }
+        http.send() //enviando a requisição http
+    })
+
+    return promise; // retorno da promessa
+}
+
+listaClientes() //chamada da função
+.then( data => { // retorno da promessa
+            data.forEach(elemento => {
+                tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+                // iterando o objeto com o método forEach() e adicionando a função criaNovaLinha(que cria <tr>) ao elemento pai <tbody> com o método appendChild()
+            })
+    })
