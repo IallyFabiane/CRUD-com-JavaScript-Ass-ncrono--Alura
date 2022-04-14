f7a4411dc14009760c9ea4591382915a6e772f27
Ially Fabiane <iallyfabiane@gmail.com>
2022-04-14 13:34:35 -0300

    adicionando comentatrio

--- projeto_inicial/admin/controller/atualizaCliente-controller.js
+++ projeto_inicial/admin/controller/atualizaCliente-controller.js
@@ -2,7 +2,7 @@ import { clienteService } from "../service/cliente-service.js"
 
 const pegaURL = new URL(window.location) //objeto URL recebendo window.location que mostra onde eu estou na tela se utilizarmos o console.log() para vizualizarmos, por exemplo
 
-const id = pegaURL.searchParams.get('id') // propriedade do protótipo do objeto URL
+const id = pegaURL.searchParams.get('id') // propriedade searchParams do protótipo do objeto URL; utilizamos o get para pegar o id
 
 const inputNome = document.querySelector('[data-nome]') // capturando o imput nome
 const inputEmail = document.querySelector('[data-email]') // capturando o imput email
